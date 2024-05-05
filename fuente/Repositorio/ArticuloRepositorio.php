c669d0bba8cc3c2f31aa9d80410d17b3e10895c4
kobakk <davidyyhj@gmail.com>
2024-05-05 17:39:11 +0200

    pedidos

--- fuente/Repositorio/ArticuloRepositorio.php
+++ fuente/Repositorio/ArticuloRepositorio.php
@@ -23,9 +23,10 @@ class ArticuloRepositorio{
     }
     public function getArticulosPorCategoria(int $idCategoria):array{
         $sql = "SELECT codigo, descripcion, pv, stock, reponerA, idCategoria 
-        FROM articulos 
+        FROM articulo 
         WHERE idCategoria = :idCategoria";
         try{
+            require_once __DIR__ . '/../../core/ConexionBd.inc';
             $con = (new ConexionBd())->getConexion();
             $stn = $con->prepare($sql);
             $stn->bindValue(":idCategoria",$idCategoria);
