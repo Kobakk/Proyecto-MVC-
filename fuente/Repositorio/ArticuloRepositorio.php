93080394a95ae2fb1c9e6a6a10c5ccd353db0038
kobakk <davidyyhj@gmail.com>
2024-05-05 20:18:12 +0200

    PedidoRepositorio/setPedido: Realizar un Insert en Pedido, ArticuloRepositorio/updatePedido: Actualiza el stock en Pedido

--- fuente/Repositorio/ArticuloRepositorio.php
+++ fuente/Repositorio/ArticuloRepositorio.php
@@ -42,8 +42,24 @@ class ArticuloRepositorio{
         }
     }
 
-    public function acutializarArticulo(){
-        $sql = "";
-        return $array = [];
+    public function acutializarArticulo(int $cantidad, string $codArticulo){
+        $sql = "UPDATE articulo 
+            SET stock = stock - :cantidad
+            WHERE codigo = :codArticulo";
+        try{
+            require_once __DIR__ . '/../../core/ConexionBd.inc';
+            $con = (new ConexionBd())->getConexion();
+            $stn = $con->prepare($sql);
+            $stn->bindValue(':cantidad',$cantidad);
+            $stn->bindValue(':codArticulo',$codArticulo);
+            $stn->execute();
+        }catch(\PDOException $ex){
+            throw $ex;          
+        }catch(\Exception $ex){
+            throw $ex;
+        }finally{
+            $con = null;
+            $stn = null;
+        }
     }
 }
--- fuente/Repositorio/PedidoRepositorio.php
+++ fuente/Repositorio/PedidoRepositorio.php
@@ -1,8 +1,26 @@
 <?php
 class PedidoRepositorio{
-    public function setPedido():array{
-        $sql = "SELECT * FROM pedido";
-        return $array = [];
+    public function setPedido(array $pedido){
+        $sql = "INSERT INTO pedido (idCliente, codArticulo,fPedido, cantidad, pv)
+        VALUES (:idCliente,:codArticulo,:fecha,:cantidad,:pv)";
+        try{
+            require_once __DIR__ . '/../../core/ConexionBd.inc';
+            $con = (new ConexionBd())->getConexion();
+            $stn = $con->prepare($sql);
+            $stn->bindValue(':idCliente',$pedido['idCliente']);
+            $stn->bindValue(':codArticulo',$pedido['codArticulo']);
+            $stn->bindValue(':fecha',$pedido['fecha']);
+            $stn->bindValue(':cantidad',$pedido['cantidad']);
+            $stn->bindValue(':pv',$pedido['total']);
+            $stn->execute();
+        }catch(\PDOException $ex){
+            throw $ex;
+        }catch(Exception $ex){
+            throw $ex;
+        }finally{
+            $con = null;
+            $stn = null;
+        }
     }
     public function setPedidoYlineaPedido():array{
         $sql = "";
